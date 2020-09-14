df91fabb1ed2885d1b09f704bcb23982b12a948c
seiya.hirata <hs62514192@yahoo.co.jp>
2020-09-14 14:05:41 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,6 +47,10 @@ class TasksController < ApplicationController
       flash[:success] = 'タスクが削除されました'
       redirect_to tasks_path
     end
+    
+    def task_params
+      params.require(:task).permit(:content, :status)
+    end  
 end
 
 private
@@ -55,6 +59,4 @@ def set_task
   @task = Task.find(params[:id])
 end  
 
-def task_params
-  params.require(:task).permit(:content, :status)
-end  
+
